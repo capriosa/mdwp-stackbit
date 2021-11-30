51463a2801e5a5fc22d2e20620edf6ca895c3b09
Stackbit Code Editor <projects@stackbit.com>
2021-11-30 10:40:47 +0000

    TestimonialCard.js: changed

--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TestimonialCard = ({ annotationPrefix, title, body, testimonial }) => {
     return (
-        <div className="w-full h-screen flex justify-center" data-sb-field-path={annotationPrefix}>
+        <div className="w-full flex justify-center" data-sb-field-path={annotationPrefix}>
             <div className="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
                 <div className="flex justify-center md:justify-end -mt-16">
                     <img className="w-20 h-20 object-cover rounded-full border-2 border-indigo-500" src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" />
