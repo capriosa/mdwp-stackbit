ea33e01f3071dd75052d5d6c79a91390f669ee5a
Stackbit Code Editor <projects@stackbit.com>
2021-11-30 08:03:59 +0000

    TestimonialCard.js: changed

--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -7,7 +7,7 @@ const TestimonialCard = ({ body }) => {
             </div>
             <div>
                 <h2 class="text-gray-800 text-3xl font-semibold">Design Tools</h2>
-                <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
+                <p class="mt-2 text-gray-600"></p>
             </div>
             <div class="flex justify-end mt-4">
                 <a href="#" class="text-xl font-medium text-indigo-500">John Doe</a>
