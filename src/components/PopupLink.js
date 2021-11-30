bf5a65abb65324ce0c09bcb9b8aee1a6aef2d806
Stackbit Code Editor <projects@stackbit.com>
2021-11-30 16:35:25 +0000

    PopupLink.js: changed

--- src/components/PopupLink.js
+++ src/components/PopupLink.js
@@ -67,7 +67,7 @@ export default function PopupLink(props) {
                 )}
             </Link>
             {popup &&
-                <div className="flex justify-center p-10 mt-2"><span><aX</span><p className="bg-gray-500 p-4 flex-shrink md:w-1/3">{props.body}</p></div>
+                <div className="flex justify-center p-10 mt-2"><span><a></a>X</span><p className="bg-gray-500 p-4 flex-shrink md:w-1/3">{props.body}</p></div>
 
             }
         </>
