657b5d06c369c46583f5afa99eec7021c02475e4
Stackbit Code Editor <projects@stackbit.com>
2021-11-30 07:56:51 +0000

    TestimonialCard.js: changed

--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -1,11 +1,7 @@
 
 const TestimonialCard = ({ body }) => {
     return (
-        <div className="py-2 bg-primary text-white text-center">
-            <div className="max-w-xl mx-auto">
-                <p>{body}</p>
-            </div>
-        </div>
+        
     )
 }
 
