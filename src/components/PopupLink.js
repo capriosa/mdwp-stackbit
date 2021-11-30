bb619601203e58a7564016b5af21c8b962f14b03
Stackbit Code Editor <projects@stackbit.com>
2021-11-30 12:41:43 +0000

    PopupLink.js: changed

--- src/components/PopupLink.js
+++ src/components/PopupLink.js
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import classNames from 'classnames';
 import Link from '@stackbit/components/dist/utils/link';
-import ArrowLeft from '@stackbit/components/distsvgs/arrow-left';
-import ArrowRight from '@stackbit/components/distsvgs/arrow-right';
-import Cart from '@stackbit/components/distsvgs/cart';
-import Facebook from '@stackbit/components/distsvgs/facebook';
-import GitHub from '@stackbit/components/distsvgs/github';
-import Instagram from '@stackbit/components/distsvgs/instagram';
-import LinkedIn from '@stackbit/components/distsvgs/linkedin';
-import Twitter from '@stackbit/components/distsvgs/twitter';
+import ArrowLeft from '@stackbit/components/dist/svgs/arrow-left';
+import ArrowRight from '@stackbit/components/dist/svgs/arrow-right';
+import Cart from '@stackbit/components/dist/svgs/cart';
+import Facebook from '@stackbit/components/dist/svgs/facebook';
+import GitHub from '@stackbit/components/dist/svgs/github';
+import Instagram from '@stackbit/components/dist/svgs/instagram';
+import LinkedIn from '@stackbit/components/dist/svgs/linkedin';
+import Twitter from '@stackbit/components/dist/svgs/twitter';
 
 const iconMap = {
     arrowLeft: ArrowLeft,
