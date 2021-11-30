c79d71913555c3ffd5e27714992ae4a6f8908d97
Stackbit Code Editor <projects@stackbit.com>
2021-11-30 14:37:49 +0000

    PopupLink.js: changed

--- src/components/PopupLink.js
+++ src/components/PopupLink.js
@@ -67,7 +67,7 @@ export default function PopupLink(props) {
             )}
         </Link>
             {popup &&
-                <p className>Lorem Ipsum Tralilala</p>
+                <p className="">Lorem Ipsum Tralilala</p>
             
             }
         </>
