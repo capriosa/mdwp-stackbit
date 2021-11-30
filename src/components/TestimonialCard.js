b8698fcece7bb498fb981efb058e57186d352d4f
Stackbit Code Editor <projects@stackbit.com>
2021-11-30 09:41:18 +0000

    TestimonialCard.js: changed

--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -7,7 +7,7 @@ const TestimonialCard = ({ title, body, testimonial }) => {
                     <img className="w-20 h-20 object-cover rounded-full border-2 border-indigo-500" src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" />
                 </div>
                 <div>
-                    <h2 className="text-gray-800 text-3xl font-semibold">{title}</h2>
+                    <h2 className="text-gray-800 text-3xl font-semibold" data-sb-field-path=".title">{title}</h2>
                     <p className="mt-2 text-gray-600">{body}</p>
                 </div>
                 <div className="flex justify-end mt-4">
