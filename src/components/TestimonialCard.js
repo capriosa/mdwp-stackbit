79992e2595bc74e200e2e863345f69d9dcf78496
Stackbit Code Editor <projects@stackbit.com>
2021-11-30 08:29:06 +0000

    TestimonialCard.js: changed

--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -1,7 +1,7 @@
 
 const TestimonialCard = ({ body }) => {
     return (
-        <div class=
+        <div class=""
         <div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
   <div class="flex justify-center md:justify-end -mt-16">
     <img class="w-20 h-20 object-cover rounded-full border-2 border-indigo-500" src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"/>
