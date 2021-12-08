8cab24cb04f2b1aef5809f7e5c45a6ecf57e9ca1
Stackbit Code Editor <projects@stackbit.com>
2021-12-08 15:51:17 +0000

    ProfileCard.js: changed

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -25,6 +25,7 @@ const ProfileCard = ({ annotationPrefix, title, body, testimonial }) => {
                         </svg>
                         Your Location - 25.0000° N, 71.0000° W
                     </p>
+         </div>
          </div>           
     );
 };
