ef10eb70df9f59f0511613e6da7a9791637bd7f2
Stackbit Code Editor <projects@stackbit.com>
2021-12-14 14:22:49 +0000

    Section.js: changed

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Markdown from 'markdown-to-jsx';
 import classNames from 'classnames';
-import { mapStylesToClassNames as mapStyles } from ''@stackbit/components/dist';
+import { mapStylesToClassNames as mapStyles } from '@stackbit/components/dist';
 
 export default function Section(props) {
     const cssId = props.elementId || null;
