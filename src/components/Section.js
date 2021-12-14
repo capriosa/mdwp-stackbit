c5f3af61184a397b7e1a590f8b32490e87afc1e7
Stackbit Code Editor <projects@stackbit.com>
2021-12-14 15:30:42 +0000

    Section.js: changed

--- src/components/Section.js
+++ src/components/Section.js
@@ -54,7 +54,7 @@ export default function Section(props) {
                     >
                         {ctaBody(props)}
                         {ctaActions(props)}
-                        
+                        {}
                     </div>
                 </div>
             </div>
