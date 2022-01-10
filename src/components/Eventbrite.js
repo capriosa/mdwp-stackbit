e0c385ce3e7a791a2d4e2d7703de36493f561131
Stackbit Code Editor <projects@stackbit.com>
2022-01-10 14:59:38 +0000

    Eventbrite.js: changed

--- src/components/Eventbrite.js
+++ src/components/Eventbrite.js
@@ -7,40 +7,40 @@ import { mapStylesToClassNames as mapStyles } from '@stackbit/components/dist/ut
 
 
 export default function Eventbrite(props) {
-const cssId = props.elementId || null;
+    const cssId = props.elementId || null;
     const colors = props.colors || 'colors-a';
     const sectionStyles = props.styles?.self || {};
     const sectionBorderWidth = sectionStyles.borderWidth ? sectionStyles.borderWidth : 0;
     const key = 'EDMO3BWIFXRUIQSDP7IZ';
     const id = '169603411369';
     const url = 'https://www.eventbriteapi.com/v3/events/' + id + '/?token=' + key + '&expand=venue';
-    const [description,setDescription] = React.useState(null);
-    const [logo,setLogo] = React.useState(null);
-    const [date,setDate] = React.useState(null);
-    const [link,setURL] = React.useState(null);
-    const [event,setEvent] = React.useState(null);
+    const [description, setDescription] = React.useState(null);
+    const [logo, setLogo] = React.useState(null);
+    const [date, setDate] = React.useState(null);
+    const [link, setURL] = React.useState(null);
+    const [event, setEvent] = React.useState(null);
     const desc_url = 'https://www.eventbriteapi.com/v3/events/' + id + '/description/?token=' + key + '&expand=venue'
 
     // NEW ??? https://www.eventbriteapi.com/v3/users/me/?token=EDMO3BWIFXRUIQSDP7IZ
 
     axios.get(desc_url).then(res => {
-      const response = res.data;
-      const event = response.description;
-      setEvent(event)
+        const response = res.data;
+        const event = response.description;
+        setEvent(event)
     })
 
     axios.get(url)
-      .then(res => {
+        .then(res => {
 
-    const response = res.data;
-    setLogo(response.logo.original.url);
-    setDescription(response.name.text);
-    setURL(response.url);
-      });
-      
+            const response = res.data;
+            setLogo(response.logo.original.url);
+            setDescription(response.name.text);
+            setURL(response.url);
+        });
+
     return (
         <div
-        id={cssId}
+            id={cssId}
             className={classNames(
                 'sb-component',
                 'sb-component-section',
@@ -60,36 +60,36 @@ const cssId = props.elementId || null;
             style={{
                 borderWidth: `${sectionBorderWidth}px`
             }}
-            >
-                <div class="px-2 py-32 bg-white md:px-0">
-  <div class="container items-center max-w-6xl px-8 mx-auto xl:px-5">
-    <div class="flex flex-wrap items-center sm:-mx-3">
-      <div class="w-full md:w-1/2 md:px-3">
-        <div class="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
-          <h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
-            {name}
-          </h1>
-          <p class="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">{description}</p>
-          <div class="relative flex flex-col sm:flex-row sm:space-x-4">
-            <a href={link} class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
-              Anmelden
+        >
+            <div class="px-2 py-32 bg-white md:px-0">
+                <div class="container items-center max-w-6xl px-8 mx-auto xl:px-5">
+                    <div class="flex flex-wrap items-center sm:-mx-3">
+                        <div class="w-full md:w-1/2 md:px-3">
+                            <div class="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
+                                <h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
+                                    {description}
+                                </h1>
+
+                                <div class="relative flex flex-col sm:flex-row sm:space-x-4">
+                                    <a href={link} class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
+                                        Anmelden
               <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
-            </a>
-            
-          </div>
+                                    </a>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="w-full md:w-1/2">
+                            <div class="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
+                                <img src={logo} />
+                            </div>
+                        </div>
+                    </div>
+                    <p mt-24 dangerouslySetInnerHTML={{ __html: event }} />
+                </div>
+
+            </div>
         </div>
-      </div>
-      <div class="w-full md:w-1/2 mt-12">
-        <div class="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
-            <img src={logo} />
-          </div>
-      </div>
-    </div>
-    <p dangerouslySetInnerHTML={{ __html: event }} />
-  </div>
-  
-</div>
-</div>
     )
 
 }
