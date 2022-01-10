c7e5d7ee39e57c8206858a681a854757fba6f19f
Stackbit Code Editor <projects@stackbit.com>
2022-01-10 14:49:51 +0000

    Eventbrite.js: changed

--- src/components/Eventbrite.js
+++ src/components/Eventbrite.js
@@ -67,7 +67,7 @@ const cssId = props.elementId || null;
       <div class="w-full md:w-1/2 md:px-3">
         <div class="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
           <h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
-            <span class="block text-indigo-600 xl:inline">{name}</span>
+            {name}</span>
           </h1>
           <p class="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">{description}</p>
           <div class="relative flex flex-col sm:flex-row sm:space-x-4">
