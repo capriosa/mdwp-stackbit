801fe42847ec5afe06f54693d0d934a02fcc18fa
Stackbit Code Editor <projects@stackbit.com>
2022-01-10 15:04:46 +0000

    Eventbrite.js: changed

--- src/components/Eventbrite.js
+++ src/components/Eventbrite.js
@@ -61,32 +61,32 @@ export default function Eventbrite(props) {
                 borderWidth: `${sectionBorderWidth}px`
             }}
         >
-            <div class="px-2 py-32 bg-white md:px-0">
-                <div class="container items-center max-w-6xl px-8 mx-auto xl:px-5">
-                    <div class="flex flex-wrap items-center sm:-mx-3">
-                        <div class="w-full md:w-1/2 md:px-3">
-                            <div class="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
-                                <h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
+            <div className="px-2 py-32 bg-white md:px-0">
+                <div className="container items-center max-w-6xl px-8 mx-auto xl:px-5">
+                    <div className="flex flex-wrap items-center sm:-mx-3">
+                        <div className="w-full md:w-1/2 md:px-3">
+                            <div className="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
+                                <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
                                     {description}
                                 </h1>
 
 
                             </div>
                         </div>
-                        <div class="w-full md:w-1/2">
-                            <div class="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
+                        <div className="w-full md:w-1/2">
+                            <div className="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
                                 <img src={logo} />
                             </div>
-                            <div class="relative mt-16 flex flex-col sm:flex-row sm:space-x-4">
-                                <a href={link} class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
+                            <div className="relative mt-16 flex flex-col sm:flex-row sm:space-x-4">
+                                <a href={link} className="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
                                     Anmelden
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
                                 </a>
 
                             </div>
                         </div>
                     </div>
-                    <p mt-16 dangerouslySetInnerHTML={{ __html: event }} />
+                    <p className="mt-16" dangerouslySetInnerHTML={{ __html: event }} />
                 </div>
 
             </div>
