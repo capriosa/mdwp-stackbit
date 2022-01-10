d05d949c2a1a31108eca4035e546c96c6e946830
Stackbit Code Editor <projects@stackbit.com>
2022-01-10 20:04:59 +0000

    Eventbrite.js: changed

--- src/components/Eventbrite.js
+++ src/components/Eventbrite.js
@@ -6,6 +6,7 @@ import { getComponent } from '@stackbit/components/dist/components-registry';
 import { mapStylesToClassNames as mapStyles } from '@stackbit/components/dist/utils/map-styles-to-class-names';
 
 
+
 export default function Eventbrite(props) {
     const cssId = props.elementId || null;
     const colors = props.colors || 'colors-a';
