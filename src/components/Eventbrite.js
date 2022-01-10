45c52b4709dc5bf4181166ce5625ad38be37d39b
Stackbit Code Editor <projects@stackbit.com>
2022-01-10 16:14:25 +0000

    Eventbrite.js: changed

--- src/components/Eventbrite.js
+++ src/components/Eventbrite.js
@@ -78,7 +78,7 @@ export default function Eventbrite(props) {
                                 <img src={logo} />
                             </div>
                             <div className="relative mt-16 flex flex-col sm:flex-row sm:space-x-4">
-                                <a href={link} className="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
+                                <a href={link} className="flex sb-component-button-primary items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
                                     Anmelden
               <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
                                 </a>
