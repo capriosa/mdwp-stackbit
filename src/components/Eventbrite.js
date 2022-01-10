832b2669c62c8b688889f69157c0688be6a285a1
Stackbit Code Editor <projects@stackbit.com>
2022-01-10 13:41:41 +0000

    Eventbrite.js: changed

--- src/components/Eventbrite.js
+++ src/components/Eventbrite.js
@@ -47,7 +47,7 @@ const cssId = props.elementId || null;
                 borderWidth: `${sectionBorderWidth}px`
             }}
             >
-            <h1>{description}</h1>   
+            <h1>{description.html}</h1>   
             </div>
     )
 
