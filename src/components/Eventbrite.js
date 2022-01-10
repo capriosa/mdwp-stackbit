76172acc9104fc6147cb57b50d1b84fea022f588
Stackbit Code Editor <projects@stackbit.com>
2022-01-10 14:05:02 +0000

    Eventbrite.js: changed

--- src/components/Eventbrite.js
+++ src/components/Eventbrite.js
@@ -64,7 +64,7 @@ const cssId = props.elementId || null;
             <h1>{description}</h1> 
             <a href={link}>Anmelden</a>
             <img src={logo} />  
-            <p>{event}</p>
+            <p>dangerouslySetInnerHTML={{ __html: props.body }}</p>
             </div>
     )
 
