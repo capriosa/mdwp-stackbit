5af0045d42198dc018e1da958c75e72b5c30953a
Stackbit Code Editor <projects@stackbit.com>
2022-01-10 20:06:22 +0000

    Eventbrite.js: changed

--- src/components/Eventbrite.js
+++ src/components/Eventbrite.js
@@ -8,6 +8,7 @@ import { mapStylesToClassNames as mapStyles } from '@stackbit/components/dist/ut
 // This function gets called at build time
 export async function getStaticProps() {
   // Call an external API endpoint to get posts
+  
   const res = await fetch('https://.../posts')
   const posts = await res.json()
 
