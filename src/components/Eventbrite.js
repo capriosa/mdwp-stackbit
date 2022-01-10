b7e6a30114f3647197fe845c3de945c2b15d510a
Stackbit Code Editor <projects@stackbit.com>
2022-01-10 13:43:06 +0000

    Eventbrite.js: changed

--- src/components/Eventbrite.js
+++ src/components/Eventbrite.js
@@ -21,7 +21,7 @@ const cssId = props.elementId || null;
       .then(res => {
 
     const response = res.data;
-    con
+    console.log(response)
     setDescription(response.description.html)
       });
       
