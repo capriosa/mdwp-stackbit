ab1550f184b7464c1651d4af7bf8f52c666377bb
M. Droste <m.droste@mdwp.de>
2022-01-11 12:10:20 +0100

    next swr

--- src/components/Eventbrite.js
+++ src/components/Eventbrite.js
@@ -2,28 +2,37 @@ import * as React from 'react';
 import Markdown from 'markdown-to-jsx';
 import classNames from 'classnames';
 import axios from 'axios';
+import useSWR from 'swr';
 import { getComponent } from '@stackbit/components/dist/components-registry';
 import { mapStylesToClassNames as mapStyles } from '@stackbit/components/dist/utils/map-styles-to-class-names';
 
-
 export default function Eventbrite(props) {
+    const fetcher = (url) => fetch(url).then((res) => res.json());
     const cssId = props.elementId || null;
     const colors = props.colors || 'colors-a';
     const sectionStyles = props.styles?.self || {};
     const sectionBorderWidth = sectionStyles.borderWidth ? sectionStyles.borderWidth : 0;
     const key = 'EDMO3BWIFXRUIQSDP7IZ';
-    const id = props.eventbriteId || "169603411369";
+    const id = props.eventbriteId || '169603411369';
     const url = 'https://www.eventbriteapi.com/v3/events/' + id + '/?token=' + key + '&expand=venue';
     const [description, setDescription] = React.useState(null);
     const [logo, setLogo] = React.useState(null);
     const [date, setDate] = React.useState(null);
     const [link, setURL] = React.useState(null);
     const [event, setEvent] = React.useState(null);
-    const desc_url = 'https://www.eventbriteapi.com/v3/events/' + id + '/description/?token=' + key + '&expand=venue'
+    const desc_url = 'https://www.eventbriteapi.com/v3/events/' + id + '/description/?token=' + key + '&expand=venue';
 
     // NEW ??? https://www.eventbriteapi.com/v3/users/me/?token=EDMO3BWIFXRUIQSDP7IZ
+    const { data, error } = useSWR(url, fetcher);
+    if (error) {
+        return <p>Failed to load Data</p>;
+    }
+    if (!data) {
+        return <p>Loading ...</p>;
+    }
+    //console.log(data);
 
-    axios.get(desc_url).then(res => {
+    /*axios.get(desc_url).then(res => {
         const response = res.data;
         const event = response.description;
         setEvent(event)
@@ -36,7 +45,7 @@ export default function Eventbrite(props) {
             setLogo(response.logo.original.url);
             setDescription(response.name.text);
             setURL(response.url);
-        });
+        });*/
 
     return (
         <div
@@ -67,32 +76,43 @@ export default function Eventbrite(props) {
                         <div className="w-full md:w-1/2 md:px-3">
                             <div className="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
                                 <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
-                                    {description}
+                                    {data.name.text}
                                 </h1>
-                                <p class="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">Für diese Veranstaltung können Sie sich auf der Website von Eventbrite anmelden.</p>
-
+                                <p className="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">{data.description.text}</p>
                             </div>
                         </div>
                         <div className="w-full md:w-1/2">
-                            <div className="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
-                                <img src={logo} />
+                            <div className="w-full overflow-hidden rounded-md shadow-xl sm:rounded-xl">
+                                <img src={data.logo.url} />
                             </div>
                             <div className="relative mt-16 flex flex-col sm:flex-row sm:space-x-4">
-                                <a href={link} className="flex sb-component-button-primary items-center w-full px-6 py-3 mb-3 text-lg text-white rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
+                                <a
+                                    href={data.url}
+                                    className="flex sb-component-button-primary items-center w-full px-6 py-3 mb-3 text-lg text-white rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto"
+                                >
                                     Anmelden
-              <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-5 h-5 ml-1"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                    >
+                                        <line x1="5" y1="12" x2="19" y2="12"></line>
+                                        <polyline points="12 5 19 12 12 19"></polyline>
+                                    </svg>
                                 </a>
-
                             </div>
                         </div>
                     </div>
                     <p className="mt-16" dangerouslySetInnerHTML={{ __html: event }} />
                 </div>
-
             </div>
         </div>
-    )
-
+    );
 }
 
 function ctaBackgroundImage(image) {
