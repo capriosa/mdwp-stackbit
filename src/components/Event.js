ec3d8389fc03bc00f18d3af3362042dadfff6ca6
Stackbit Code Editor <projects@stackbit.com>
2022-01-12 16:02:18 +0000

    Event.js: changed

--- src/components/Event.js
+++ src/components/Event.js
@@ -24,7 +24,7 @@ export default function Event(props) {
                 <div className="flex flex-wrap items-center sm:-mx-3">
                     <div className="w-full md:w-1/2 md:px-3">
                         <div className="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
-                            <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
+                            <h1 className="text-2xl font-extrabold tracking-tight text-gray-900">
                                 {data.name.text}
                             </h1>
                             <p className="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">{data.description.text}</p>
