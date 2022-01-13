4b1c6299d9c46f1bb04f7150d9e0693b50a3d210
Stackbit Code Editor <projects@stackbit.com>
2022-01-13 19:55:40 +0000

    ImageBlock.js: changed

--- src/components/ImageBlock.js
+++ src/components/ImageBlock.js
@@ -17,7 +17,7 @@ export default function ImageBlock(props) {
     return (
         <Fade top>
         <Image
-            src='/carla.jpg'
+            src={/carla.jpg'
             layout="fill"
         />
         </Fade>
