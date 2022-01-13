c77612349c8dee773838222d30e60a911b0f7ea4
Stackbit Code Editor <projects@stackbit.com>
2022-01-13 16:07:49 +0000

    next.config.js: changed

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ module.exports = withBundleAnalyzer({
         ignoreDuringBuilds: true
     },
     images: {
-    domains: ['https://d33wubrfki0l68.cloudfront.net/'],
+    domains: ['d33wubrfki0l68.cloudfront.net/'],
   },
     webpack: (config, { webpack, dev }) => {
         // temporary: for local development -  edit package.json "@stackbit/components": "file:../stackbit-components" and uncomment the alias below.
