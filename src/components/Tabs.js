fa91ca9323eb349ecde682e56d9cae467c3ff18b
Stackbit Code Editor <projects@stackbit.com>
2022-01-17 14:03:22 +0000

    Tabs.js: changed

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -24,7 +24,7 @@ const iconMap = {
 
 export default function Tabs(props) {
     const [openTab, setOpenTab] = React.useState(1);
-    console.log(props)
+    console.log(props.tabtitle)
     return (
         <>
             <div className="flex flex-wrap justify-center">
