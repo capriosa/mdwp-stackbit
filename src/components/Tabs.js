a0d160e7dced6bcceb083d022bb0183dbfb2cff4
Stackbit Code Editor <projects@stackbit.com>
2022-01-17 14:24:21 +0000

    Tabs.js: changed

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -24,7 +24,7 @@ const iconMap = {
 
 export default function Tabs(props) {
     const [openTab, setOpenTab] = React.useState(1);
-    console.log(props.tabs[0])
+
     return (
 
         <>
@@ -37,11 +37,11 @@ export default function Tabs(props) {
                     >
                         {
                             props.tabs.map((tab, index) =>
-                                <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                                <li key={index} className="-mb-px mr-2 last:mr-0 flex-auto text-center">
                                     <a
                                         className={
                                             "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
-                                            (openTab === 1
+                                            (openTab === index + 1
                                                 ? "text-white bg-indigo-600"
                                                 : "text-black bg-white")
                                         }
@@ -62,38 +62,16 @@ export default function Tabs(props) {
                     <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
                         <div className="px-4 py-5 flex-auto">
                             <div className="tab-content tab-space">
-                                <div className={openTab === 1 ? "block" : "hidden"} id="link1">
-                                    <p>
-                                        Collaboratively administrate empowered markets via
-                                        plug-and-play networks. Dynamically procrastinate B2C users
-                                        after installed base benefits.
-                    <br />
-                                        <br /> Dramatically visualize customer directed convergence
-                                        without revolutionary ROI.
-                  </p>
-                                </div>
-                                <div className={openTab === 2 ? "block" : "hidden"} id="link2">
-                                    <p>
-                                        Completely synergize resource taxing relationships via
-                                        premier niche markets. Professionally cultivate one-to-one
-                                        customer service with robust ideas.
-                    <br />
-                                        <br />
-                                        Dynamically innovate resource-leveling customer service for
-                                        state of the art customer service.
-                  </p>
-                                </div>
-                                <div className={openTab === 3 ? "block" : "hidden"} id="link3">
-                                    <p>
-                                        Efficiently unleash cross-media information without
-                                        cross-media value. Quickly maximize timely deliverables for
-                                        real-time schemas.
-                    <br />
-                                        <br /> Dramatically maintain clicks-and-mortar solutions
-                                        without functional solutions.
-                  </p>
-                                </div>
+                                {
+                                    props.item.map((item, index) =>
+                                        <div className={openTab === index + 1 ? "block" : "hidden"} id="link1">
+                                            <p>
+                                                {item}
+                                            </p>
+                                        </div>
+                                    )}
                             </div>
+
                         </div>
                     </div>
                 </div>
