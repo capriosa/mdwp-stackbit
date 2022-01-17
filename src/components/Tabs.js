b4400e8b03ddfccdfb97b3e69caa0e820222329d
Stackbit Code Editor <projects@stackbit.com>
2022-01-17 14:15:29 +0000

    Tabs.js: changed

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -28,11 +28,6 @@ export default function Tabs(props) {
     return (
 
         <>
-            {
-                props.tabs.map((tab, index) =>
-                    <div>{tab[index]}</div>
-                )
-            }
 
             <div className="flex flex-wrap justify-center">
                 <div className="w-3/4">
@@ -40,63 +35,29 @@ export default function Tabs(props) {
                         className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row"
                         role="tablist"
                     >
-                        <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
-                            <a
-                                className={
-                                    "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
-                                    (openTab === 1
-                                        ? "text-white bg-indigo-600"
-                                        : "text-black bg-white")
-                                }
-                                onClick={e => {
-                                    e.preventDefault();
-                                    setOpenTab(1);
-                                }}
-                                data-toggle="tab"
-                                href="#link1"
-                                role="tablist"
-                            >
-                                Profile
-              </a>
-                        </li>
-                        <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
-                            <a
-                                className={
-                                    "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
-                                    (openTab === 2
-                                        ? "text-white bg-indigo-600"
-                                        : "text-black bg-white")
-                                }
-                                onClick={e => {
-                                    e.preventDefault();
-                                    setOpenTab(2);
-                                }}
-                                data-toggle="tab"
-                                href="#link2"
-                                role="tablist"
-                            >
-                                Settings
-              </a>
-                        </li>
-                        <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
-                            <a
-                                className={
-                                    "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
-                                    (openTab === 3
-                                        ? "text-white bg-indigo-600"
-                                        : "text-black bg-white")
-                                }
-                                onClick={e => {
-                                    e.preventDefault();
-                                    setOpenTab(3);
-                                }}
-                                data-toggle="tab"
-                                href="#link3"
-                                role="tablist"
-                            >
-                                Options
-              </a>
-                        </li>
+                        {
+                            props.tabs.map((tab, index) =>
+                                <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                                    <a
+                                        className={
+                                            "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
+                                            (openTab === 1
+                                                ? "text-white bg-indigo-600"
+                                                : "text-black bg-white")
+                                        }
+                                        onClick={e => {
+                                            e.preventDefault();
+                                            setOpenTab(1);
+                                        }}
+                                        data-toggle="tab"
+                                        href="#link1"
+                                        role="tablist"
+                                    >
+                                        {tab[index]}
+                                    </a>
+                                </li>
+                            )}
+
                     </ul>
                     <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
                         <div className="px-4 py-5 flex-auto">
