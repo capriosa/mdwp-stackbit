7825e8ebef9e13c3bbeca9b6afcbd867c46a143e
Stackbit Code Editor <projects@stackbit.com>
2022-01-17 10:56:47 +0000

    PopupLink.js: changed

--- src/components/PopupLink.js
+++ src/components/PopupLink.js
@@ -46,7 +46,7 @@ export default function PopupLink(props) {
     return (
         <section className="flex justify-center h-60">
             <button
-                className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                className="bg-pink-500 h-6 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                 type="button"
                 onClick={() => setShowModal(true)}
             >
