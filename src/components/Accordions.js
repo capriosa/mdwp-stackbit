b749141e62779ca05b68efd03cb20329868a6fdd
Stackbit Code Editor <projects@stackbit.com>
2022-01-18 14:03:42 +0000

    Accordions.js: changed

--- src/components/Accordions.js
+++ src/components/Accordions.js
@@ -54,12 +54,18 @@ export default function Accordions(props) {
                                         role="tablist"
                                         data-sb-field-path=".tab"
                                     >
+                                        {openTab === index ?
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z" clip-rule="evenodd" />
+                                            </svg> :
+                                            <span>
+                                                <svg xmlns="http://www.w3.org/2000/svg" className="inline-block mr-3 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z" clip-rule="evenodd" />
+                                                </svg>
 
-                                        <span>
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="inline-block mr-3 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z" clip-rule="evenodd" />
-                                            </svg>
-                                        </span>
+
+                                            </span>
+                                        }
                                         <span>{tab}</span>
                                     </a>
                                     <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-3 shadow-lg rounded-b-sm">
