bada7a28d2a7c85c6c4ba205cc11233562aeaf1c
Stackbit Code Editor <projects@stackbit.com>
2022-01-18 14:26:14 +0000

    ProfileCard.js: changed

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 const ProfileCard = ({ annotationPrefix, name, job, location, description, button, facebook, twitter, github, unsplash, dribbble, instagram, youtube }) => {
     return (
-        <blockquote class="flex items-center justify-center  col-span-1 p-6 mt-4 bg-white ">
-            <div class="flex flex-col pr-10 w-1/2 rounded-lg shadow">
+        <blockquote class="flex items-center justify-center  w-1/2 col-span-1 p-6 mt-4 bg-white rounded-lg shadow">
+            <div class="flex flex-col pr-10 w-1/2 ">
                 <div class="relative pl-12">
                     <svg class="absolute left-0 w-10 h-10 text-indigo-500 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 125">
                         <path d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z"></path>
