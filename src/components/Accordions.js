d16c126688dc48f48f148a2d846dd7a041db9114
Stackbit Code Editor <projects@stackbit.com>
2022-01-18 12:01:16 +0000

    Accordions.js: changed

--- src/components/Accordions.js
+++ src/components/Accordions.js
@@ -56,7 +56,7 @@ export default function Accordions(props) {
                                     >
                                         {tab}
                                     </a>
-                                    <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+                                    <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-1 shadow-lg rounded">
                                         <div className="px-4 py-5 flex-auto">
                                             <div className="tab-content tab-space">
 
