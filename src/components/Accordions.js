f52223c47bcbc6dab339af69d88fc34e20f3d9e0
Stackbit Code Editor <projects@stackbit.com>
2022-01-18 14:43:36 +0000

    Accordions.js: changed

--- src/components/Accordions.js
+++ src/components/Accordions.js
@@ -37,7 +37,7 @@ export default function Accordions(props) {
                     >
                         {
                             props.accordions.map((tab, index) =>
-                                <li key={index} className="-mb-px mr-2  flex-auto transition-all">
+                                <li key={index} className="-mb-px flex-auto transition-all">
                                     <a
                                         className={
                                             "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded-t-sm  block leading-normal " +
