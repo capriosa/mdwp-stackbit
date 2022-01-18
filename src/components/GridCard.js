88a4083033ce29ae127fa93c8d997b666a0ca26d
Stackbit Code Editor <projects@stackbit.com>
2022-01-18 15:37:52 +0000

    GridCard.js: changed

--- src/components/GridCard.js
+++ src/components/GridCard.js
@@ -1,15 +1,7 @@
 import * as React from 'react';
 import { useState } from 'react';
 import classNames from 'classnames';
-import Link from '@stackbit/components/dist/utils/link';
-import ArrowLeft from '@stackbit/components/dist/svgs/arrow-left';
-import ArrowRight from '@stackbit/components/dist/svgs/arrow-right';
-import Cart from '@stackbit/components/dist/svgs/cart';
-import Facebook from '@stackbit/components/dist/svgs/facebook';
-import GitHub from '@stackbit/components/dist/svgs/github';
-import Instagram from '@stackbit/components/dist/svgs/instagram';
-import LinkedIn from '@stackbit/components/dist/svgs/linkedin';
-import Twitter from '@stackbit/components/dist/svgs/twitter';
+
 
 
 
