3803af94c33c90c5c5d2f4be4443d4c1f5ab6f46
Stackbit Code Editor <projects@stackbit.com>
2022-01-18 14:27:40 +0000

    ProfileCard.js: changed

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -3,22 +3,23 @@ import React from 'react';
 
 const ProfileCard = ({ annotationPrefix, name, job, location, description, button, facebook, twitter, github, unsplash, dribbble, instagram, youtube }) => {
     return (
-        <blockquote class="flex items-center justify-center  w-1/2 col-span-1 p-6 mt-4 bg-white rounded-lg shadow">
-            <div class="flex flex-col pr-10 w-1/2 ">
-                <div class="relative pl-12">
-                    <svg class="absolute left-0 w-10 h-10 text-indigo-500 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 125">
-                        <path d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z"></path>
-                    </svg>
-                    <p class="mt-2 text-sm text-gray-600 sm:text-base lg:text-sm xl:text-base">I can't express enough, how amazing this service has been for my company.</p>
-                </div>
-                <h3 class="pl-12 mt-3 text-sm font-medium leading-5 text-gray-800 truncate sm:text-base lg:text-sm lg:text-base">
-                    John Doe
+        <div>
+            <blockquote class="flex items-center justify-center  w-1/2 col-span-1 p-6 mt-4 bg-white rounded-lg shadow">
+                <div class="flex flex-col pr-10 w-1/2 ">
+                    <div class="relative pl-12">
+                        <svg class="absolute left-0 w-10 h-10 text-indigo-500 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 125">
+                            <path d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z"></path>
+                        </svg>
+                        <p class="mt-2 text-sm text-gray-600 sm:text-base lg:text-sm xl:text-base">I can't express enough, how amazing this service has been for my company.</p>
+                    </div>
+                    <h3 class="pl-12 mt-3 text-sm font-medium leading-5 text-gray-800 truncate sm:text-base lg:text-sm lg:text-base">
+                        John Doe
                             <span class="mt-1 text-sm leading-5 text-gray-500 truncate">- CEO SomeCompany</span>
-                </h3>
-                <p class="mt-1 text-sm leading-5 text-gray-500 truncate"></p>
-            </div>
-            <img class="flex-shrink-0 w-24 h-24 bg-gray-300 rounded-full" src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;aauto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60" alt="" />
-        </blockquote>
+                    </h3>
+                    <p class="mt-1 text-sm leading-5 text-gray-500 truncate"></p>
+                </div>
+                <img class="flex-shrink-0 w-24 h-24 bg-gray-300 rounded-full" src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;aauto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60" alt="" />
+            </blockquote></div>
     );
 };
 
