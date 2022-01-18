5474c224d68ec28bc20aff44e3aec9e87fcd7de1
Stackbit Code Editor <projects@stackbit.com>
2022-01-18 12:47:17 +0000

    Accordions.js: changed

--- src/components/Accordions.js
+++ src/components/Accordions.js
@@ -55,7 +55,7 @@ export default function Accordions(props) {
                                         data-sb-field-path=".tab"
                                     >
                                         {tab}
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                        <svg className="text-right" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z" clip-rule="evenodd" />
                                         </svg>
                                     </a>
