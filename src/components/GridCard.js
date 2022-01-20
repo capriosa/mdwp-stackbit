d49a859456748746875ce95f9736e47c593c7fa8
Stackbit Code Editor <projects@stackbit.com>
2022-01-20 12:31:17 +0000

    GridCard.js: changed

--- src/components/GridCard.js
+++ src/components/GridCard.js
@@ -31,7 +31,7 @@ function Grid() {
             <div>
                 <div class="flex flex-wrap -mx-1 lg:-mx-4">
 
-                    <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                    <div class="my-1 px-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
 
 
                         <article class="overflow-hidden rounded-lg shadow-lg">
