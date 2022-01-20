47100402d427b4211c68d55ce2fe246fdabc2857
Stackbit Code Editor <projects@stackbit.com>
2022-01-20 12:30:46 +0000

    GridCard.js: changed

--- src/components/GridCard.js
+++ src/components/GridCard.js
@@ -28,7 +28,7 @@ export default function GridCard(props) {
 function Grid() {
     return (
         <>
-            <div class="container my-12 mx-auto px-4 md:px-12">
+            <div>
                 <div class="flex flex-wrap -mx-1 lg:-mx-4">
 
                     <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
