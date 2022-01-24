783b601351bf59d6d983920579cfea85b72f7523
Stackbit Code Editor <projects@stackbit.com>
2022-01-24 15:01:18 +0000

    register-components.js: changed

--- src/components/register-components.js
+++ src/components/register-components.js
@@ -18,7 +18,8 @@ registerComponents({
     Accordions: dynamic(() => import('./Accordions')),
     Eventbrite: dynamic(() => import('./Eventbrite')),
     Section: dynamic(() => import('./Section')),
-    Cart: dynamic(() => import('./Cart'))
+    Cart: dynamic(() => import('./Cart')),
+    CodeSection: dynamic(() => import('./CodeSection'))
     //ImageBlock: dynamic(() => import('./ImageBlock')),
 });
 
