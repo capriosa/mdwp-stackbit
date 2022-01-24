8efb873d3aadad2f7e5585863a4daf856d31ec53
M. Droste <m.droste@mdwp.de>
2022-01-24 23:31:01 +0100

    test

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    plugins: {
-        'postcss-import': {},
-        'postcss-nested': {},
-        tailwindcss: {},
-        autoprefixer: {}
-    }
+    plugins: [
+        // ...
+        require('tailwindcss'),
+        require('autoprefixer')
+        // ...
+    ]
 };
