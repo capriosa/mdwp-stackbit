694ff567a2f75827b0dc3e7ba6fbeb886dc7d435
M. Droste <m.droste@mdwp.de>
2022-01-25 12:02:14 +0100

    debug

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const themeStyle = require('./content/data/style.json');
 
 module.exports = {
     enabled: process.env.NODE_ENV === 'production',
-    presets: [require('@stackbit/components/styles/tailwind.default.config.js')],
+    //presets: [require('@stackbit/components/styles/tailwind.default.config.js')],
     content: ['./src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {
