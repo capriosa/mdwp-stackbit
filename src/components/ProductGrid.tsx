849ef97bf46dae246f591b8b99a442545c9906c4
Stackbit Code Editor <projects@stackbit.com>
2022-02-16 16:23:48 +0000

    ProductGrid.tsx: changed

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -10,7 +10,7 @@ import { withRemoteDataUpdates } from 'sourcebit-target-next/with-remote-data-up
 import { getComponent } from '@stackbit/components';
 
 const ProductGrid = ({ products }) => {
-    //if (!products || products.length === 0) return null;
+    if (!products || products.length === 0) return null;
     console.log(products)
     return (
         <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
