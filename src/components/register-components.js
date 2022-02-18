e9665545cb4dcae31f2e5d12d5cb10a1450cda48
M. Droste <m.droste@mdwp.de>
2022-02-18 13:18:38 +0100

    merge

--- src/components/register-components.js
+++ src/components/register-components.js
@@ -19,8 +19,8 @@ registerComponents({
     //Eventbrite: dynamic(() => import('./Eventbrite')),
     //Section: dynamic(() => import('./Section')),
     //Cart: dynamic(() => import('./Cart')),
-    CodeSection: dynamic(() => import('./CodeSection')),
-    ProductGrid: dynamic(() => import('./ProductGrid'))
+    CodeSection: dynamic(() => import('./CodeSection'))
+    //ProductGrid: dynamic(() => import('./ProductGrid'))
     //ImageBlock: dynamic(() => import('./ImageBlock')),
 });
 
